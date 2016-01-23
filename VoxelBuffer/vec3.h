4c6a166ea22098f950706436b26a49afd169109a
Aaron <RosenbergerGA1@gcc.edu>
2016-01-23 15:13:31 -0500

    Initial solution

---
+++ VoxelBuffer/vec3.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/**
+ * These were made just to keep the parameter lists of the VoxelBuffer functions from being really long.
+ * 
+ * CDB
+ */
+ 
+struct vec3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct ivec3 {
+	int x;
+	int y;
+	int z;
+};
